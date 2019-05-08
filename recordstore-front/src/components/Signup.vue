33d4c9d80791da45fe41c136bae581273546accc
td94 <quynhtrangdo94@gmail.com>
2019-05-07 21:15:50 -0400

    Fix bug in signup component

--- recordstore-front/src/components/Signup.vue
+++ recordstore-front/src/components/Signup.vue
@@ -1,30 +1,27 @@
 <template>
   <div class="max-w-sm m-auto my-8">
-    <div class="border p-10 boder-grey-light shadow rounded">
-      <h3 class="text-2xl mb-6 text-grey-darkst">Sign Up</h3>
+    <div class="border p-10 border-grey-light shadow rounded">
+      <h3 class="text-2xl mb-6 text-grey-darkest">Sign Up</h3>
       <form @submit.prevent="signup">
         <div class="text-red" v-if="error">{{ error }}</div>
+
         <div class="mb-6">
-          <label for="email" class="label">Email Address</label>
-          <input type="email" v-model="email" class="input" id="email" placeholder="example@mail.com">
+          <label for="email" class="label">E-mail Address</label>
+          <input type="email" v-model="email" class="input" id="email" placeholder="andy@web-crunch.com">
         </div>
+
         <div class="mb-6">
           <label for="password" class="label">Password</label>
-          <input type="password" v-model="password" class="input" id="password">
+          <input type="password" v-model="password" class="input" id="password" placeholder="Password">
         </div>
+
         <div class="mb-6">
-          <label for="password" class="label">Confirm Password</label>
-          <input type="password" v-model="password_confirmation" class="input" id="password_confirmation">
+          <label for="password_confirmation" class="label">Password Confirmation</label>
+          <input type="password" v-model="password_confirmation" class="input" id="password_confirmation" placeholder="Password Confirmation">
         </div>
-        <button
-          type="submit"
-          class="font-sans font-bold px-4 rounded cursor-pointer
-          no-underline bg-green hover:bg-green-dark block w-full
-          py-4 text-white items-center justify-center"
-        >Sign Up</button>
-        <div class="my-4">
-          <router-link to="/" class="link">Sign In</router-link>
-        </div>
+        <button type="submit" class="font-sans font-bold px-4 rounded cursor-pointer no-underline bg-green hover:bg-green-dark block w-full py-4 text-white items-center justify-center">Sign Up</button>
+
+        <div class="my-4"><router-link to="/" class="link-grey">Sign In</router-link></div>
       </form>
     </div>
   </div>
@@ -42,33 +39,34 @@ export default {
     }
   },
   created () {
-    this.checkSignedIn()
+    this.checkedSignedIn()
   },
   updated () {
-    this.checkSignedIn()
+    this.checkedSignedIn()
   },
   methods: {
-    signin () {
+    signup () {
       this.$http.plain.post('/signup', { email: this.email, password: this.password, password_confirmation: this.password_confirmation })
-        .then(response => this.signInSuccessful(response))
-        .catch(error => this.signinFailed(error))
+        .then(response => this.signupSuccessful(response))
+        .catch(error => this.signupFailed(error))
     },
-    signInSuccessful (response) {
+    signupSuccessful (response) {
       if (!response.data.csrf) {
-        this.signinFailed(response)
+        this.signupFailed(response)
         return
       }
+
       localStorage.csrf = response.data.csrf
       localStorage.signedIn = true
       this.error = ''
       this.$router.replace('/records')
     },
-    signinFailed (error) {
+    signupFailed (error) {
       this.error = (error.response && error.response.data && error.response.data.error) || 'Something went wrong'
       delete localStorage.csrf
       delete localStorage.signedIn
     },
-    checkSignedIn () {
+    checkedSignedIn () {
       if (localStorage.signedIn) {
         this.$router.replace('/records')
       }
